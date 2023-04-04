ad8db56574b62e0e9ccbcb0402e0af9688f75a4e
pro-top-star <topstar991122@gmail.com>
2023-04-05 03:07:52 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import ViteComponents from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt3-start-project',
+      link: [{ rel: 'icon', type: 'image/*', href: '/favicon.svg' }]
+    }
+  },
+  modules: [
+    'nuxt-windicss',
+    '@pinia/nuxt',
+    'unplugin-icons/nuxt',
+    '@vueuse/nuxt',
+    [
+      '@intlify/nuxt3',
+      {
+        localeDir: 'locales',
+        vueI18n: {
+          locale: 'en'
+        }
+      }
+    ]
+  ],
+  vite: {
+    plugins: [
+      ViteComponents({
+        resolvers: [
+          IconsResolver({
+            componentPrefix: ''
+          })
+        ],
+        dts: true
+      })
+    ]
+  }
+})
